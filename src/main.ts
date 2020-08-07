e10b26deb05124eed0e940a43b21c9a973efd4a7
shippokun <okashi0tabetai@gmail.com>
2020-08-07 23:19:32 +0900

    feat: 🎸 add graphql config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import { apolloClient } from './graphql/client';
 
 createApp(App)
   .use(router)
+  .provide(DefaultApolloClient, apolloClient)
   .mount('#app');
